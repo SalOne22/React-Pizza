5378924b6c3ccf52d573e40b1f675a2df2698f14
Vladyslav Vladinov <vladdf44@gmail.com>
2022-10-26 23:56:38 +0200

    Закончил 4 серию курса React Pizza, сделал сайт более интерактивным и добавил генерацию пицц по файлу json

--- src/components/PizzaBlock.jsx
+++ src/components/PizzaBlock.jsx
@@ -1,26 +1,38 @@
-function PizzaBlock() {
+import { useState } from 'react';
+
+function PizzaBlock({ title, price, imageUrl, sizes, types }) {
+  const [sizeIndex, setSizeIndex] = useState(0);
+  const [typeIndex, setTypeIndex] = useState(0);
+
   return (
     <div className="pizza-block">
-      <img
-        className="pizza-block__image"
-        src="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
-        alt="Pizza"
-      />
-      <h4 className="pizza-block__title">Чизбургер-пицца</h4>
+      <img className="pizza-block__image" src={imageUrl} alt="Pizza" />
+      <h4 className="pizza-block__title">{title}</h4>
       <div className="pizza-block__selector">
         <ul>
-          <li className="active">тонкое</li>
-          <li>традиционное</li>
+          {types.map((type, index) => (
+            <li
+              onClick={() => setTypeIndex(index)}
+              className={typeIndex === index ? 'active' : ''}
+            >
+              {type === 0 ? 'Тонкое' : 'Традиционное'}
+            </li>
+          ))}
         </ul>
         <ul>
-          <li className="active">26 см.</li>
-          <li>30 см.</li>
-          <li>40 см.</li>
+          {sizes.map((size, index) => (
+            <li
+              onClick={() => setSizeIndex(index)}
+              className={sizeIndex === index ? 'active' : ''}
+            >
+              {size} см.
+            </li>
+          ))}
         </ul>
       </div>
       <div className="pizza-block__bottom">
-        <div className="pizza-block__price">от 395 ₽</div>
-        <div className="button button--outline button--add">
+        <div className="pizza-block__price">от {price} ₽</div>
+        <button className="button button--outline button--add">
           <svg
             width="12"
             height="12"
@@ -34,8 +46,8 @@ function PizzaBlock() {
             />
           </svg>
           <span>Добавить</span>
-          <i>2</i>
-        </div>
+          <i>0</i>
+        </button>
       </div>
     </div>
   );
